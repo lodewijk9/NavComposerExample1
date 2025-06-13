e929c61e909b477bf818acccfdebb74321205cb5
Lodewijk <lodewijkpa88@outlook.com>
2025-06-12 23:59:22 -0500

    deep links: * Used to navigate to the application from outside or from inside if you have a complex navigation (sometimes is use a deeplink is the best rather than navigation navHost) * AndroidManifest : you have to write the intent filter that like the template. your deeplink will follow * LoginScreen : has a button to prove an intent to navegate using a local deeplink(currently this button is not working properly is redirecting to the browser rather than your app the same appens with details/{name}) * NavigationWrapper : shows how to navigate an specific UI in your app and how pass parameters through the URI.
    Note: In old emulator you need to add the link manually and also by working locally

--- app/src/main/java/com/example/navcomposeexample1/LoginScreen.kt
+++ app/src/main/java/com/example/navcomposeexample1/LoginScreen.kt
@@ -1,5 +1,6 @@
 package com.example.navcomposeexample1
 
+import android.content.Intent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -8,10 +9,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.sp
+import androidx.core.net.toUri
 
 @Composable
 fun LoginScreen(NavigateToHome: () -> Unit) {
+    val context = LocalContext.current
     Column (
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
@@ -22,6 +26,16 @@ fun LoginScreen(NavigateToHome: () -> Unit) {
         Button(onClick = { NavigateToHome() }) {
             Text(
                 text = "Go to home"
+            )
+        }
+        // using a deeplink from inside the application
+        Button(onClick = {
+            val uri = "https://lodewijk.dev/home"
+            val intent = Intent(Intent.ACTION_VIEW, uri.toUri())
+            context.startActivity(intent)
+        }) {
+            Text(
+                text = "deep link go to Home"
             )
         }
         Spacer(modifier = Modifier.weight(1f))
--- app/src/main/java/com/example/navcomposeexample1/core/navigation/NavigationWrapper.kt
+++ app/src/main/java/com/example/navcomposeexample1/core/navigation/NavigationWrapper.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navDeepLink
 import androidx.navigation.toRoute
 import com.example.navcomposeexample1.DetailScreen
 import com.example.navcomposeexample1.HomeScreen
@@ -19,15 +20,17 @@ fun NavigationWrapper() {
         composable<Login>{
             LoginScreen{ navController.navigate(Home)}
         }
-
-        composable<Home> {
+        // by using this in home you can configure your deeplink such that if link match with this navigate to this UI
+        composable<Home>(deepLinks = listOf(navDeepLink { uriPattern = "https://lodewijk.dev/home" }) )  {
             HomeScreen{ messageToDetail -> navController.navigate(Detail(name = messageToDetail)) }
         }
+        // to prove this deep link go to Tools/App Links Assistant/ select test on device or emulator
+        composable<Detail>(deepLinks = listOf(navDeepLink { uriPattern = "https://lodewijk.dev/detail/{name}" })) { backStackEntry ->
+//            val detail:Detail = backStackEntry.toRoute<Detail>()
+            val name = backStackEntry.arguments?.getString("name").orEmpty()
 
-        composable<Detail> { backStackEntry ->
-            val detail:Detail = backStackEntry.toRoute<Detail>()
             DetailScreen(
-                name = detail.name,
+                name = name,
                 navigateBack = {
                     /* back one in nav stack */
                       //  navController.popBackStack()
